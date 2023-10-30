2f0227168d64108167ef16b32796d7da5261c126
Ebaneo <ebaneo_89@hotmail.com>
2023-10-30 14:44:33 +0100

    main function working

--- lab code/main.m
+++ lab code/main.m
@@ -1,29 +1,12 @@
-% Define test inputs
-M = 64;   % Number of rows
-N = 64;   % Number of columns
-P = 32;   % Number of frames
-G = 20;   % Number of directions
+load test_data.mat;
+signal = atti( :, :, :, abs(bi-1000)<100 );
+gi = gi( abs(bi-1000)<100, : );
 
-% Create a synthetic signal (replace this with your actual signal data)
-signal = rand(M, N, P, G);
-
-% Generate synthetic gradient directions
-% For simplicity, we'll create a random set of unit vectors
-gi = randn(G, 3);
-gi = gi ./ vecnorm(gi, 2, 2);  % Normalize vectors to unit length
-
-% Optional parameters (you can customize these as needed)
-options = struct();
-options.L = 6;         % Maximum order of SH (default: 6)
-options.lambda = 0.006; % Tikhonov regularization parameter (default: 0.006)
-options.chunksz = 1000; % Chunk size for processing (default: 1000)
-options.mask = true(M, N, P);  % Voxel mask (default: entire image)
-
-
-suma = varar(1,2,'L', options.L, 'lambda', options.lambda);
-
+[M,N,P,G] = size(signal);
+options = create_signal2sh_options(M, N, P);
+options.mask=mask;
 % Call the signal2sh function
-%SH = signal2sh(signal, gi, 'L', options.L, 'lambda', options.lambda, 'chunksz', options.chunksz, 'mask', options.mask);
+SH = signal2sh(signal, gi, options);
 
 % Display the size of the resulting SH coefficients
 disp(size(SH));
