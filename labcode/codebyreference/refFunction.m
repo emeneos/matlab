977a568abcc68d2c91edc1f8be449bdfadfaa43f
Ebaneo <ebaneo_89@hotmail.com>
2023-11-14 12:25:39 +0100

    use variable input in init_array

--- labcode/codebyreference/refFunction.m
+++ labcode/codebyreference/refFunction.m
@@ -1,5 +1,6 @@
-function Y = refFunction()
-    Y = zeros(5, 10);
+function Y = refFunction(Y)
+    coder.varsize('Y', [inf, inf], [1 1]);
+    assert(isa(Y, 'double'));
     coder.cinclude("init_array.h");
     coder.ceval('init_array', coder.wref(Y), int32(numel(Y)));
 end
